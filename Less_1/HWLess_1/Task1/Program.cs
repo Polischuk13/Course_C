6fe836bed0c32c57d3af35abcbf062b596ca0825
Artem Polischuk <fedpol1316@mail.ru>
2023-02-24 21:35:29 +0300

    Task1

---
+++ Less_1/HWLess_1/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите значение 1: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение 2: ");
+int b = int.Parse(Console.ReadLine()!);
+if(a < b)
+{
+    Console.Write("min = ");
+    Console.WriteLine(a);
+    Console.Write("max = ");
+    Console.Write(b);
+}
+else if(a > b)
+{
+    Console.Write("min = ");
+    Console.WriteLine(b);
+    Console.Write("max = ");
+    Console.Write(a);
+}
+else if (a == b)
+{
+    Console.Write("error");   
+}
