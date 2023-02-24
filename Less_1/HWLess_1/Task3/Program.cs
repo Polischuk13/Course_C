1c47b58480ca0e16993a8459945471baf0fede09
Artem Polischuk <fedpol1316@mail.ru>
2023-02-24 22:25:58 +0300

    Task3

---
+++ Less_1/HWLess_1/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите значение 1: ");
+float a = int.Parse(Console.ReadLine()!);
+float b = 2;
+if(a % b == 0)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
