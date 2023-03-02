5304a7ca80c1f59facc7d5ba421713c8cb209e87
Artem Polischuk <fedpol1316@mail.ru>
2023-03-02 11:16:23 +0300

    Task3_3

---
+++ Less_3/3_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// 3. Напишите программу, которая принимает на вход число (N) 
+//    и выдаёт таблицу квадратов чисел от 1 до N.
+
+int num = int.Parse(Console.ReadLine()!);
+
+void Div(int n)
+{
+    int i = 1;
+
+    while(n>=i)
+    {
+        Console.WriteLine(Math.Pow(i,2));
+        i++;
+
+    }
+}
+
+Div(num);
