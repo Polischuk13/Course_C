20ee4235f453d84ccfed672f390d49bcdcc3b6c6
Artem Polischuk <fedpol1316@mail.ru>
2023-03-04 15:18:26 +0300

    2Task2

---
+++ Less_2/HWLess_2/2Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿void Yes (int Number)
+{
+    if (Number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return;
+    }  
+    while (Number > 999)
+    {
+        Number = Number /= 10;
+    }
+   Console.WriteLine(Number % 10); 
+}
+
+Yes(int.Parse(Console.ReadLine()!));
