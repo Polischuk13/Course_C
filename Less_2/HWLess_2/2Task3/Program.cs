5051af9ac30cd1477be70e930f432e9a8542830b
Artem Polischuk <fedpol1316@mail.ru>
2023-03-04 15:20:43 +0300

    2Task3

---
+++ Less_2/HWLess_2/2Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿void Weekend (int num)
+
+{
+    string text = "no";
+    if (num == 7 || num == 6) text = "Weekend"  ;
+    else if (num < 6 && num > 0) text = "Workday";
+    else text = " not a week day";
+    Console.WriteLine($"{num} -> {text}");
+}
+Weekend(int.Parse(Console.ReadLine()!));
