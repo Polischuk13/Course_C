36cee17876669ef5259a5b9bb3b1b6e77c1eaf1f
Artem Polischuk <fedpol1316@mail.ru>
2023-03-06 00:05:40 +0300

    add nullish value check and prompt to initial input

--- Less_3/HWLess_3/3Task1/Program.cs
+++ Less_3/HWLess_3/3Task1/Program.cs
@@ -1,9 +1,17 @@
-﻿void Palindrom (string numbers)
+﻿Console.WriteLine("Введите пятизначное число ");
+void Palindrom (string numbers)
 {
+        
+    if(string.IsNullOrEmpty(numbers))
+        {
+        Console.WriteLine("Ввод не должен быть пустым ");
+        Palindrom(Console.ReadLine()); 
+        return;
+        }
     if (numbers[0] == numbers[4] && numbers[1] == numbers[3])
-    {
+        {
         Console.WriteLine("Да");
-    }
+        }
     else Console.WriteLine("Нет");
-}
+} 
 Palindrom(Console.ReadLine());
