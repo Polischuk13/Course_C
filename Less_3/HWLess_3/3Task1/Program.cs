fa357e1df8acbe4808d4453a71eb079c8a7de81d
Artem Polischuk <fedpol1316@mail.ru>
2023-03-05 20:56:41 +0300

    smth

---
+++ Less_3/HWLess_3/3Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿void Palindrom (string numbers)
+{
+    if (numbers[0] == numbers[4] && numbers[1] == numbers[3])
+    {
+        Console.WriteLine("Да");
+    }
+    else Console.WriteLine("Нет");
+}
+Palindrom(Console.ReadLine());
