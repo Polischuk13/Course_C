57ede74ee2f81bcb2fbc5ffe16d4185574838e6b
Artem Polischuk <fedpol1316@mail.ru>
2023-03-06 11:22:41 +0300

    add_lesson_4

---
+++ Less_4/4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 2. Напишите программу, которая принимает на вход
+//    число N и выдаёт произведение чисел от 1 до N.
+
+int CompositionNum(int num)
+{
+    int mutiply = 1;
+    for(int i = 1; num >= i; i++)
+        mutiply *= i;
+
+    return mutiply;
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = CompositionNum(A);
+Console.WriteLine(result);
---
+++ Less_4/4_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//// 3. Напишите программу, которая выводит массив из 8 элементов,
+//    заполненный нулями и единицами в случайном порядке.
+
+void massive(int mas){
+    int[] numbers =  new int[mas];
+    for (int i = 0; i < mas; i++)
+    {
+        numbers[i] = new Random().Next(2);
+        Console.WriteLine(numbers[i]);
+    }
+}
+
+massive(8);
