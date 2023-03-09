1b8fb470dd038583f062af1222137495ca04763f
Artem Polischuk <fedpol1316@mail.ru>
2023-03-09 11:37:31 +0300

    Less5Done

---
+++ Less_5/5_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// 3. Задайте одномерный массив из 123 случайных чисел.
+//    Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+
+int RetNum(int[] Mass, int from, int to)
+
+{
+    int count = 0;
+    for (int i = 0; i < Mass.Length; i++)
+    {
+        if (Mass[i] >= from & Mass[i] <= to)
+            count = count + 1;
+
+    }
+    return count;
+}
+
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+
+int first = int.Parse(Console.ReadLine()!);
+int sec = int.Parse(Console.ReadLine()!);
+
+int result = RetNum(mass, first, sec);
+
+Console.WriteLine(result);
---
+++ Less_5/5_4/Program.cs
@@ -0,0 +1,60 @@
+﻿// 4. Найдите произведение пар чисел в одномерном массиве.
+//    Парой считаем первый и последний элемент, второй и предпоследний
+//    и т.д. Результат запишите в новом массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+// void MultiMas(int[] arr){
+//     int mult = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         for (int j = arr.Length-1; j > 0; j--){
+//             mult = arr[i]*arr[j];
+//             Console.WriteLine($"{arr[i]} x {arr[j]} = {mult}");
+//         }
+//     }
+
+//     //Переделаю, под диктовку)
+// }
+
+int[] MultiMas(int[] arr){
+    int size = arr.Length;
+    int[] newMass = new int[size/2 + size%2];
+    for (int i = 0; i < size/2; i++)
+    {
+        newMass[i] = arr[i]*arr[size-1-i];
+    }
+    if (size%2 == 1){
+        newMass[newMass.Length-1] = arr[arr.Length/2];    
+    }
+    return newMass;
+}
+
+Console.WriteLine("Введите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Диапазон от");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Диапазон до");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+
+int[] newNewMass = MultiMas(mass);
+Print(newNewMass); 
