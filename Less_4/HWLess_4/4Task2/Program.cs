e5907545a93b75bbe2e8bb06533bdb06a0776177
Artem Polischuk <fedpol1316@mail.ru>
2023-03-12 11:11:18 +0300

    HW4

---
+++ Less_4/HWLess_4/4Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumNum(int num)
+{
+    int length = 1;
+    int res = 0;
+    for (int i = 0; i < length; i++)
+    {
+        int num1 = num / 1000;
+        int num2 = num / 100 % 10;
+        int num3 = num / 10 % 10;
+        int num4 = num % 10;
+        res = num1 + num2 + num3 + num4;
+        return res;
+    }
+    return res;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result =  SumNum(A);
+Console.WriteLine(result);
---
+++ Less_4/HWLess_4/4Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+Console.WriteLine("Введите длину массива: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите стартовое значение: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конечное значение: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
