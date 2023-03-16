1deb7881915deb15507ab608038e6bead9155356
Artem Polischuk <fedpol1316@mail.ru>
2023-03-16 11:47:04 +0300

    Less_7

---
+++ Less_7/7_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1. Задайте двумерный массив. Найдите элементы,
+//    у которых обе позиции чётные, и замените эти элементы на их квадраты.
+
+void EvenPos(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 1; i < row; i+=2)
+        for (int j = 1; j < column; j+=2)
+            arr[i, j] = arr[i,j] * arr[i, j];
+
+}
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+EvenPos(mass);
+
+Print(mass);
---
+++ Less_7/7_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// 4. Задайте двумерный массив. Введите элемент, и найдите первое его вхождение,
+//    выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
+
+string SearchNum(int[,] arr, int num)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+
+            if (arr[i,j] == num)
+             return $"[{i+1},{j+1}]";
+    return "Такого числа нет";
+        
+                
+}
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+int val = int.Parse(Console.ReadLine()!);
+
+string result = SearchNum(mass, val);
+Console.WriteLine(result);
