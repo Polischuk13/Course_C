fbabf5276e66b5bd301e76a3800e9cd60e6eaf03
Artem Polischuk <fedpol1316@mail.ru>
2023-03-20 12:33:28 +0300

    Less_8

---
+++ Less_8/8_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. Задайте двумерный массив. Напишите программу,
+//    которая поменяет местами первую и последнюю строку массива.
+
+void ReplaceIndex(int[,] array)
+{
+    int ArrayRow = array.GetLength(0);
+    int ArrayColumn = array.GetLength(1);
+    
+    for (int j = 0; j < ArrayColumn; j++)
+    {
+        (array[0,j] , array[ArrayRow - 1 ,j]) = (array[ArrayRow - 1, j], array[0,j]);
+    }
+}
