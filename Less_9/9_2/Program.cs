c6a9d727e3db4d46a163b6dcfdbcbe464d9dfa18
Artem Polischuk <fedpol1316@mail.ru>
2023-03-23 11:43:25 +0300

    9_2

---
+++ Less_9/9_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// 3. Напишите программу, которая будет принимать на вход число
+//    и возвращать сумму его цифр. Использовать рекурсию.
+
+int SumDigits(int N)
+{
+    if(N==0) return 0;
+    
+    return SumDigits(N / 10) + N%10;
+}
+
+int N = int.Parse(Console.ReadLine()!);
+Console.Write(SumDigits(N));
