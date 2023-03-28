d4d3bffef1fc831c40c2ec1415a39482c4900fe1
Artem Polischuk <fedpol1316@mail.ru>
2023-03-28 16:42:35 +0300

    9Task1

---
+++ Less_9/HWLess_9/9Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все чётные
+//натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void Rec( int M, int N)
+{
+    if (M > N) return;
+    else if (N % 2 == 0)
+    {
+        Rec(M, N - 2);
+        Console.Write($"{N}, ");
+    }
+    else if (N % 2 != 0)
+    {
+        N -= 1;
+        Rec(M, N - 2);
+        Console.Write($"{N}, ");
+    }
+    
+}
+int M = int.Parse(Console.ReadLine()!);
+int N = int.Parse(Console.ReadLine()!);
+Rec(M, N);
